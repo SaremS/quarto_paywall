1f39b59c9a1bf25806b7381873a51e0451fb3ff7
Sarem Seitz <sarem.seitz@gmail.com>
2023-12-02 07:39:36 +0100

    more docs

--- rust_server/src/purchase/mod.rs
+++ rust_server/src/purchase/mod.rs
@@ -1,4 +1,7 @@
+///Main abstraction to handle purchases/payment processing
 pub mod purchase_handler;
+
+///Handling stripe payments
 pub mod stripe_purchase_handler;
 
 pub use purchase_handler::*;
--- rust_server/src/routes/mod.rs
+++ rust_server/src/routes/mod.rs
@@ -1,4 +1,11 @@
+///All functionality regarding user sign-up, login, confirmation and deletion
 pub mod auth;
+
+///All functionality around purchasing an item (including payment provider webhooks)
 pub mod purchase;
+
+///All functionality around static files
 pub mod static_files;
+
+///All functionality around email and general user communication
 pub mod mail;
