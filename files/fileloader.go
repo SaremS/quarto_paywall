0d44dc300a2b72dbad13c90babfc6c3b509b544c
Sarem Seitz <sarem.seitz@gmail.com>
2024-07-30 21:37:47 +0200

    start cleaning up

---
+++ files/fileloader.go
@@ -0,0 +1,39 @@
+package files
+
+import (
+	"io/ioutil"
+	log "github.com/go-pkgz/lgr"
+)
+
+type FileLoader interface {
+	ReadFileToString(path string) (string, error)
+}
+
+type DiskFileLoader struct{}
+
+func newDiskFileLoader() *DiskFileLoader {
+	return &DiskFileLoader{}
+}
+
+func (d *DiskFileLoader) ReadFileToString(path string) (string, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Failed to fetch and load content in file %s: %v", path, err)
+		return "", err
+	}
+	return string(data), nil
+}
+
+type DummyFileLoader struct{
+	pseudoString string
+}
+
+func newDummyFileLoader(pseudoString string) *DummyFileLoader {
+	return &DummyFileLoader{
+		pseudoString: pseudoString,
+	}
+}
+
+func (d *DummyFileLoader) ReadFileToString(path string) (string, error) {
+	return d.pseudoString, nil
+}
---
+++ files/recursive_loader.go
@@ -0,0 +1,68 @@
+package files
+
+import (
+	log "github.com/go-pkgz/lgr"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+
+type RecursiveLoader interface {
+	WalkTarget(targetPath string, fileType string) (map[string]*string, error)
+}
+
+type RecursiveFilePathLoader struct {
+	fileLoader FileLoader
+}
+
+func newRecursiveFilePathLoader(fileLoader FileLoader) *RecursiveFilePathLoader {
+	return &RecursiveFilePathLoader{
+		fileLoader: fileLoader,
+	}
+}
+
+type DummyPathLoader struct {
+	fileLoader DummyFileLoader
+}
+
+func newDummyPathLoader(pseudoString string) *DummyPathLoader {
+	return &DummyPathLoader{
+		fileLoader: *newDummyFileLoader(pseudoString),
+	}
+}
+
+func (r *RecursiveFilePathLoader) WalkTarget(targetPath string, fileType string) (map[string]*string, error) {
+	targetMap := make(map[string]*string)
+	err := filepath.Walk(targetPath, func(path string, info os.FileInfo, err error) error {
+		if strings.HasSuffix(path, fileType) {
+			log.Printf("Loaded path: %s", path)
+
+			content, err := r.fileLoader.ReadFileToString(path)
+			if err != nil {
+				return err
+			}
+
+			targetMap[path] = &content
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return targetMap, err
+}
+
+func (r *DummyPathLoader) WalkTarget(targetPath string, fileType string) (map[string]*string, error) {
+	targetMap := make(map[string]*string)
+
+	content, err := r.fileLoader.ReadFileToString(targetPath)
+	if err != nil {
+		return targetMap, err
+	}
+
+	targetMap[targetPath] = &content
+
+	return targetMap, nil
+}
